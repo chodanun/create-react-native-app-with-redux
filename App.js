0cb7998b248525c86b59da128ea6f94400a16497
Chodanun Srinil <step_nilanam@hotmail.com>
2017-06-19 00:08:25 +0700

    initializing redux

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, combineReduxers, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger'
 
 export default class App extends React.Component {
   render() {
+
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-        <Text>NAT</Text>
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
